8dbca8d2c09ec11dd2af04d4f269981eb9440d39
padmed <ddarchinian@gmail.com>
2022-12-17 00:40:44 +0400

    Make message  appear on user interaction

--- logic.js
+++ logic.js
@@ -1,5 +1,4 @@
 const rePassword = document.getElementById("rep-pswd");
-
 rePassword.addEventListener('input', (event) => {
     const password = document.getElementById("pswd");
     // console.log(`pswd ${password.value}`);
@@ -13,5 +12,29 @@ rePassword.addEventListener('input', (event) => {
     } else {
         event.target.setCustomValidity("Error");
     }
-
 })
+
+window.addEventListener('click', (event) => {
+    if(['name', 'mail', 'pswd', 'rep-pswd'].includes(event.target.id)){
+        const input = event.target;
+        const prompt = document.querySelector(`span[data-prompt="${input.getAttribute('data-prompt')}"`);
+
+        clearPromts();
+        prompt.style.display = 'block';
+    }
+})
+
+
+function clearPromts() {
+    const prompts = document.getElementsByClassName('prompt');
+
+    for (let i = 0; i < prompts.length; i++) {
+        const prompt = prompts[i];
+
+        prompt.style.display = 'none';
+    }
+}
+
+
+
+
